a30f660d2e8e50791daf1bd7b359150369120ce2
Ian Lancaster <ianclancaster@gmail.com>
2016-10-08 12:20:16 -0600

    sets up testing describe and contexts

--- test/controller-test.js
+++ test/controller-test.js
@@ -1,1 +1,15 @@
 const assert = require('chai').assert
+
+// describe('Controller', function(){
+//   context('Feature', function(){
+//     it('Controller feature test', function(){
+//     });
+//   });
+// });
+
+describe('Controller', function(){
+  context('Unit', function(){
+    it('Controller unit test', function(){
+    });
+  });
+});
--- test/todo-test.js
+++ test/todo-test.js
@@ -1,1 +1,15 @@
 const assert = require('chai').assert
+
+// describe('Todo', function(){
+//   context('Feature', function(){
+//     it('Todo feature test', function(){
+//     });
+//   });
+// });
+
+describe('Todo', function(){
+  context('Unit', function(){
+    it('Todo unit test', function(){
+    });
+  });
+});
